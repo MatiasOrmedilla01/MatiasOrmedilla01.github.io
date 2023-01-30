24119ffddb46d1d10e5e59cef622e8faf83f8f9b
MatiasOrmedilla01 <123853001+MatiasOrmedilla01@users.noreply.github.com>
2023-01-30 01:03:35 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,143 @@
+// // Simulador: pedir cuantas veces salió campeón Argentina hasta que ingresen la opción correcta
+
+// let nombreUsuario = prompt("Bienvenido a la web del campeón del mundo. \n Ingrese su nombre:") ;
+
+// alert(`¡Hola, ${nombreUsuario}, empecemos!`) ;
+
+
+// let argentinaUser=parseInt(prompt("Ingrese cuantas veces Argentina Salió campeón del mundo:"));
+// const argentinaCampeon = 3;
+
+// for(let i=0; i<2; i++){
+//   if(argentinaUser==argentinaCampeon){
+//         alert("¡Correcto! Argneina salió campeón en 3 ocaciones: 1978, 1986 y 2022");
+//         break;
+//     } else{
+//         argentinaUser=(prompt("Dato incorrecto. Vuelva a intentarlo"));
+//     }
+// }
+let nombreUsuario = prompt("Bienvenido a la web del campeón del mundo. \n Ingrese su nombre:") ;
+
+alert(`¡Hola, ${nombreUsuario}, empecemos!`) ;
+
+let argentina = prompt("Ingrese la selección para ver el resultado vs Argnetina en la última copa mundial: arabia saudita, mexico, polonia, australia, paises bajos, croacia, francia");
+
+switch(argentina){
+    case "arabia saudita":
+    alert("Derrota de Argentina 1-2");
+    break;
+
+    case "mexico":
+    alert("Victoria de Argentina 2-0");
+    break;
+
+    case "polonia":
+    alert("Victoria de Argentina 2-0");
+    break;
+
+    case "australia":
+    alert("Victoria de Argentina 1-0");
+    break;
+
+    case "paises bajos":
+    alert("Victoria de Argentina por penales");
+    break;
+
+    case "croacia":
+    alert("Victoria de Argentina 3-0");
+    break;
+
+    case "francia":
+    alert("Victoria de Argentina por penales. CAMPEONES DEL MUNDO");
+    break;
+    default:
+        alert("Este país no jugó vs Argentina");
+        break;
+    
+}
+
+alert(`¡Gracias por visitar nuestra web ${nombreUsuario}, Vuelva pronto!`) ;
+
+
+
+
+//  let pais =prompt("Ingese un país");
+
+//     if (pais == "ecuador"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "qatar"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "iran"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "gales"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "mexico"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "arabia saudita"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "tunez"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "dinamarca"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "alemania"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "costa rica"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "belgica"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "canada"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "camerun"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "serbia"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "uruguay"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "ghana"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "ghana"){
+//             alert(pais + " quedo eliminado en fase de grupos");
+//         }else if (pais == "estados unidos"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "australia"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "japon"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "corea del sur"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "polonia"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "senegal"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "españa"){
+//             alert(pais + " quedo eliminado octavos de final");
+//                 }else if (pais == "australia"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "suiza"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "marruecos"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "portugal"){
+//             alert(pais + " quedo eliminado cuartos de final");
+//         }else if (pais == "inglaterra"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "brasil"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "paises bajos"){
+//             alert(pais + " quedo eliminado octavos de final");
+//         }else if (pais == "croacia"){
+//             alert(pais + " quedo eliminado en semi final");
+//         }else if (pais == "marruecos"){
+//             alert(pais + " quedo eliminado en semi final");
+//         }else if (pais == "francia"){
+//             alert(pais + " perdió la final");
+//         }else if (pais == "argentina"){
+//             alert(pais + " CAMPEÓN DEL MUNDO");
+//         }else{
+//                 alert("Error: la selección que seleccionaste no jugó el mundial/está mal escrita");
+                
+            
+//     }
+    
+
+
